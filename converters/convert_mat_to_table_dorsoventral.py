dfb1ce94412624dcf710c68062d47f3184436747
aarslan <aarslan@g6.(none)>
2014-01-18 13:51:45 -0500

    asd

--- converters/convert_mat_to_table_dorsoventral.py
+++ converters/convert_mat_to_table_dorsoventral.py
@@ -16,7 +16,7 @@ import h5py
 def create_empty_table(table_fname):
     
     class images(ta.IsDescription):
-        features     = ta.Float64Col(shape = (1196))
+        features     = ta.Float64Col(shape = (1140))
         actions        = ta.StringCol(64)
 	names        = ta.StringCol(64)
 
